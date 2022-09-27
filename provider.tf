2b626309c9d7fbf3e03922e54eb40e78df19c08f
tudormangri <110907736+tudormangri10@users.noreply.github.com>
2022-09-27 15:40:30 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,8 @@
-provider "github" {
-  token = var.token
-  owner = var.owner
-}
-
 terraform {
-  required_version = "~> 1.0"
-
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 4.0"
+      version = "5.2.0"
     }
   }
 }
