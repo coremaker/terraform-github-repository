f5b4d7904b5fd48b7c6ff7693acaa67dc51798f5
Tudor Mangri <tudor@coremaker.io>
2022-10-03 15:36:08 +0300

    Removed contexts from branch_protection resource

--- main.tf
+++ main.tf
@@ -48,7 +48,6 @@ resource "github_branch_protection" "branch_protection" {
 
     content {
       strict   = required_status_checks.value.strict
-      # contexts = required_status_checks.value.contexts
     }
   }
 
