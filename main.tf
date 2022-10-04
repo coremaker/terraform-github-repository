fe8b9bbd25b99d243c08bf2131c35ea96e31c25e
Tudor Mangri <tudor@coremaker.io>
2022-10-04 12:15:29 +0300

    Formatted the code

--- main.tf
+++ main.tf
@@ -1,20 +1,18 @@
-
 # Repository configuration module
 resource "github_repository" "coremaker_repo" {
-  name         = var.repo_name
-  description  = var.repo_description
-  auto_init    = true
+  name        = var.repo_name
+  description = var.repo_description
+  auto_init   = true
 
-  visibility   = var.repo_visibility
+  visibility = var.repo_visibility
 }
-
 
 # Add teams to the given repository
 resource "github_team_repository" "coremaker_team_repo" {
   repository = github_repository.coremaker_repo.name
-  count = "${length(var.teams)}"
+  count      = length(var.teams)
 
-  team_id = var.teams[count.index].team_name
+  team_id    = var.teams[count.index].team_name
   permission = var.teams[count.index].permission
 }
 
@@ -22,7 +20,7 @@ resource "github_team_repository" "coremaker_team_repo" {
 resource "github_branch_protection" "branch_protection" {
   repository_id = github_repository.coremaker_repo.node_id
 
-  for_each = var.branch_protection
+  for_each                        = var.branch_protection
   pattern                         = each.key
   enforce_admins                  = each.value.enforce_admins
   require_signed_commits          = each.value.require_signed_commits
@@ -36,7 +34,7 @@ resource "github_branch_protection" "branch_protection" {
     for_each = each.value.required_status_checks
 
     content {
-      strict   = required_status_checks.value.strict
+      strict = required_status_checks.value.strict
     }
   }
 
@@ -44,9 +42,9 @@ resource "github_branch_protection" "branch_protection" {
     for_each = each.value.required_pull_request_reviews
 
     content {
-      dismiss_stale_reviews             = required_pull_request_reviews.value.dismiss_stale_reviews
-      require_code_owner_reviews        = required_pull_request_reviews.value.require_code_owner_reviews
-      required_approving_review_count   = required_pull_request_reviews.value.required_approving_review_count
+      dismiss_stale_reviews           = required_pull_request_reviews.value.dismiss_stale_reviews
+      require_code_owner_reviews      = required_pull_request_reviews.value.require_code_owner_reviews
+      required_approving_review_count = required_pull_request_reviews.value.required_approving_review_count
     }
   }
 }
@@ -55,7 +53,7 @@ resource "github_branch_protection" "branch_protection" {
 resource "github_repository_deploy_key" "example_repository_deploy_key" {
   title      = "Repository keys"
   repository = github_repository.coremaker_repo.name
-  for_each = toset(var.deploy_key)
+  for_each   = toset(var.deploy_key)
   key        = each.value
   read_only  = "false"
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "repo_name" {
-    description = "GitHub Repository Name"
-    default     = ""
+  description = "GitHub Repository Name"
+  default     = ""
 }
 
 variable "repo_description" {
-    default = ""
+  default = ""
 }
 
 variable "repo_visibility" {
-    default = ""
+  default = ""
 }
 
 variable "teams" {
@@ -20,21 +20,21 @@ variable "teams" {
 
 variable "branch_protection" {
   type = map(object({
-        enforce_admins                  = bool    # true = status checks for repository administrators.
-        require_signed_commits          = bool    # true = requires all commits to be signed with GPG.
-        required_linear_history         = bool    # true = enforces a linear commit Git history, which prevents anyone from pushing merge commits to a branch
-        require_conversation_resolution = bool    # true = requires all conversations on code must be resolved before a pull request can be merged.
-        allows_deletions                = bool    # true = allows the branch to be deleted.
-        allows_force_pushes             = bool    # true = allows force pushes on the branch.
-        blocks_creations                = bool    # true = blocks creating the branch.
-        required_status_checks = list(object({
-          strict       = bool          # Require branches to be up to date before merging. Defaults to false
-        }))
-        required_pull_request_reviews = list(object({
-          dismiss_stale_reviews             = bool   # Dismiss approved reviews automatically when a new commit is pushed. Defaults to false
-          require_code_owner_reviews        = bool   # Require an approved review in pull requests including files with a designated code owner. Defaults to false.
-          required_approving_review_count   = number # Require x number of approvals to satisfy branch protection requirements
-        }))
+    enforce_admins                  = bool # true = status checks for repository administrators.
+    require_signed_commits          = bool # true = requires all commits to be signed with GPG.
+    required_linear_history         = bool # true = enforces a linear commit Git history, which prevents anyone from pushing merge commits to a branch
+    require_conversation_resolution = bool # true = requires all conversations on code must be resolved before a pull request can be merged.
+    allows_deletions                = bool # true = allows the branch to be deleted.
+    allows_force_pushes             = bool # true = allows force pushes on the branch.
+    blocks_creations                = bool # true = blocks creating the branch.
+    required_status_checks = list(object({
+      strict = bool # Require branches to be up to date before merging. Defaults to false
+    }))
+    required_pull_request_reviews = list(object({
+      dismiss_stale_reviews           = bool   # Dismiss approved reviews automatically when a new commit is pushed. Defaults to false
+      require_code_owner_reviews      = bool   # Require an approved review in pull requests including files with a designated code owner. Defaults to false.
+      required_approving_review_count = number # Require x number of approvals to satisfy branch protection requirements
+    }))
 
   }))
   description = "Branch protections for selected branches in repository"
