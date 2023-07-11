3099cfaf5461e0838de47bc8279b4acb85ded3a9
Cosmin Briscaru <cosmin@coremaker.io>
2023-07-11 13:42:38 +0300

    run pre commit

--- examples/list-ex.tf
+++ examples/list-ex.tf
@@ -1,32 +1,32 @@
 variable "github_repositories" {
   type = list(object({
-    name                     = string
+    name        = string
     description = optional(string, "")
-    visibility = optional(string, "public")
+    visibility  = optional(string, "public")
     teams = optional(list(object({
       team_name  = string
       permission = string
     })))
     branch_protection = optional(map(object({
       enforce_admins                  = optional(bool, true)
-      require_signed_commits          = optional(bool, false) 
-      required_linear_history         = optional(bool, false) 
-      require_conversation_resolution = optional(bool, true) 
-      allows_deletions                = optional(bool, false) 
-      allows_force_pushes             = optional(bool, true) 
-      blocks_creations                = optional(bool, false) 
+      require_signed_commits          = optional(bool, false)
+      required_linear_history         = optional(bool, false)
+      require_conversation_resolution = optional(bool, true)
+      allows_deletions                = optional(bool, false)
+      allows_force_pushes             = optional(bool, true)
+      blocks_creations                = optional(bool, false)
       required_status_checks = optional(list(object({
         strict = optional(bool, true) # Require branches to be up to date before merging. Defaults to false
-      })),[{strict = true}])
+      })), [{ strict = true }])
       required_pull_request_reviews = optional(list(object({
-        dismiss_stale_reviews           = optional(bool, true)   
-        require_code_owner_reviews      = optional(bool, false)   
-        required_approving_review_count = optional(number, 2) 
-      })),[{dismiss_stale_reviews = true, require_code_owner_reviews = false, required_approving_review_count = 2}])
+        dismiss_stale_reviews           = optional(bool, true)
+        require_code_owner_reviews      = optional(bool, false)
+        required_approving_review_count = optional(number, 2)
+      })), [{ dismiss_stale_reviews = true, require_code_owner_reviews = false, required_approving_review_count = 2 }])
     })))
     deploy_key = optional(list(string), [])
   }))
-  default     = []
+  default = []
   # Value example
   # value = [
   #   {
@@ -53,19 +53,19 @@ locals {
   }
 }
 
-module "github_repository" {
-  source = "../"
+module "github_repository_list" {
+  source   = "../"
   for_each = local.repos
-  
+
   # Repository Configuration
-  repo_name = each.key
+  repo_name        = each.key
   repo_description = each.value["description"]
-  repo_visibility = each.value["visibility"]
+  repo_visibility  = each.value["visibility"]
 
   # Added teams to the repository in question
   repo_teams = each.value["teams"]
 
   # Multiple Branch Protection 
   branch_protection = each.value["branch_protection"]
-  deploy_key = each.value["deploy_key"]
+  deploy_key        = each.value["deploy_key"]
 }
--- examples/providers.tf
+++ examples/providers.tf
@@ -1,11 +1,21 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "5.2.0"
+    }
+  }
+}
+
+
 provider "github" {
   owner = "coremaker"
 
   # Authentication using user token
-  token = "---GITHUB_USER_TOKEN---"
+  # token = "---GITHUB_USER_TOKEN---"
 
   # Authentication using app_auth
-  app_auth { 
+  app_auth {
     id              = "---GITHUB_APP_ID---"
     installation_id = "---GITHUB_APP_INSTALLATION_ID---"
     pem_file        = "---GITHUB_APP_PEM_FILE---"
